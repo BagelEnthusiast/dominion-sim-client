ff9e7954120c4778ed6ab6742c694ef5ca5a1dd6
Nathan Richardson <nathan.richardson94@gmail.com>
2023-07-22 22:57:30 -0500

    data coming through in new api format

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Card } from './components/Card';
 
-interface ApiData {
-  users: User[];
-}
-
 interface User {
   name: string
   age: string
@@ -15,89 +11,87 @@ interface CardSet {
   cards: Array<string>
 }
 
+interface ShoppingListItem {
+  card: string,
+  quantity: number
+}
 
-async function getUsersAsync(): Promise<User[]> {
+interface Strategy {
+  label: string
+  shoppingList: ShoppingListItem[]
+}
+
+interface ApiData {
+  [user: string]: {
+    strategies: Strategy[]
+  }
+}
+
+async function getUserStrategiesAsync(username: string): Promise<Strategy[]> {
   const response = await fetch('http://localhost:3000/api')
   const apiData = (await response.json()) as unknown as ApiData
-  return apiData.users
+  return apiData[username].strategies
 }
 
 async function getCardDataAsync(): Promise<string[]> {
   const response = await fetch('https://dominion-sim-api.onrender.com/cards')
   const dominionApiData = (await response.json()) as unknown as CardSet[]
-  const allCards = dominionApiData.map(cs => cs.cards).flat();
-  console.log(dominionApiData)
+  const allCards = dominionApiData.map(cs => cs.cards).flat()
+  console.log(allCards)
   return allCards
 }
 
 export function DeckApp() {
-  const [list, setList] = useState<string[] | undefined>();
-  const [cardList, setCardList] = useState<string[] | undefined>();
+  const [cardList, setCardList] = useState<string[] | undefined>()
+
+  //hard coding default user until login is setup
+  const [username, setUsername] = useState('paul')
+  const [strategies, setStrategies] = useState <Strategy[] | undefined>()
 
   useEffect(() => {
-    getUsersAsync()
-      .then(users => {
-        const names = users.map(u => u.name);
-        setList(names);
+    getUserStrategiesAsync(username)
+      .then(strats => {
+        setStrategies(strats)
       })
       .catch(err => console.log(err));
     getCardDataAsync()
       .then(cards => { 
         setCardList(cards)
       })
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
   }, []);
 
  
-  if (!list) {
-    return <h1>loading</h1>;
+  if (!cardList) {
+    return <h1>loading</h1>
   }
   
   return (
     <>
-      {list.map((name, index) => {
-        return (
-          <div key={`list-${index}`}>
-            <h2>{name}</h2>
-          </div>
-        );
-      })
-      
-      }
-      {cardList && cardList.map((card, index) => {
+      {//removed conditional render... do we need it?
+      cardList.map((card, index) => {
         return (
           <div key={`card-${index}`}>
             <Card name={card}></Card>
           </div>
         )
-      })}
+      })
+      }
+      {
+        strategies && strategies.map((strat, index) => {
+          return (
+            <div key={`strat-${index}`}>
+              {strat.label}
+            </div>
+          )
+        })
+      }
     </>
   )
 }
 
-
-// const obj: any = {
-//   "paul": {
-//     "strategies": [{
-//       "label": 'bigmoney',
-//       "shoppingList": [
-//         { card: 'Province', quantity: 12, },
-//         { card: 'Gold', quantity: 10, },
-//         { card: 'Silver', quantity: 10, },
-//       ],
-//     }, {
-//       "label": 'chapel shit',
-//       "shoppingList": [
-//         { card: 'Chapel', quantity: 1, },
-//         { card: 'Province', quantity: 12, },
-//         { card: 'Gold', quantity: 10, },
-//         { card: 'Silver', quantity: 10, },],
-//     }],
-//   },
-//   "nathan": {
-//     "strategies": [{
-//       "label": 'gold',
-//       "shoppingList": [],
-//     }],
-//   },
-// }
+/*
+todo
+make strategy component
+add strategy functionality
+*/
