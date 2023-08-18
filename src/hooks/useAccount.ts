c29818f1a2d7ede3d4a13ddd924feeee28fe1981
BagelEnthusiast <nathan.richardson94@gmail.com>
2023-08-17 21:01:17 -0500

    switching from cookie to query string

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,11 +1,14 @@
-import Cookies from 'js-cookie'
 import * as jose from 'jose'
 import { JwtPayload } from "../interfaces"
+import { useLocation } from 'react-router-dom'
 
 export function useAccount() {
-  if (!Cookies.get('access-token')) return null
+  const token = useLocation().search
+  console.log('token: ', token)
 
-  const token: string = Cookies.get('access-token') as string
+  if (!token) return null
+  
+  //const token: string = Cookies.get('access-token') as string
   //found jose.decodeJwt on these docs: https://github.com/panva/jose/blob/e2836e6aaaddecde053018884abb040908f186fd/docs/functions/util_decode_jwt.decodeJwt.md
   const payload: JwtPayload = jose.decodeJwt(token) as unknown as JwtPayload
   const username = payload.email
